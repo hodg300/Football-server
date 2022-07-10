334edd551b45775a0fdddf3bca2e1926aada1ef3
hod <hod.gohasi@hotmail.com>
2022-07-10 23:37:42 +0300

    * Deleted - credentials.json * Added - credentials.json to heroku

--- PlayersDB/Google_sheet_handler.py
+++ PlayersDB/Google_sheet_handler.py
@@ -17,8 +17,8 @@ class GoogleSheetHandler(BaseHandlerInterface):
         super().__init__()
 
         # add credentials to the account
-        credentialsPath = op.join(op.dirname(__file__),'credentials.json')
-        creds = ServiceAccountCredentials.from_json_keyfile_name(credentialsPath, SCOPE)
+        # credentialsPath = op.join(op.dirname(__file__),'credentials.json')
+        creds = ServiceAccountCredentials.from_json_keyfile_name(os.environ['GOOGLE_APPLICATION_CREDENTIALS'], SCOPE)
         # authorize the clientsheet
         self.client = gspread.authorize(creds)
 
